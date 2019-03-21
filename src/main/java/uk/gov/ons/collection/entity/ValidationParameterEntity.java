03c6950665da4d832542e467ff654e7a41cdc887
dllmorgan <github@ghost.me.uk>
2019-03-21 10:35:45 +0000

    Removed persistence layer annotations

--- src/main/java/uk/gov/ons/collection/entity/ValidationParameterEntity.java
+++ src/main/java/uk/gov/ons/collection/entity/ValidationParameterEntity.java
@@ -1,50 +1,23 @@
 package uk.gov.ons.collection.entity;
 
 import com.fasterxml.jackson.annotation.JsonBackReference;
-import io.swagger.annotations.ApiModel;
 import lombok.*;
-
-import javax.persistence.*;
 import java.sql.Timestamp;
 
-@Entity
 @Getter
 @Setter
 @NoArgsConstructor
 @RequiredArgsConstructor
 @ToString
-@Table(name = "validationparameter", schema = "dev01")
-@ApiModel(value = "ValidationParameter", description = "A ValidationParameter entity")
 public class ValidationParameterEntity {
-
     @JsonBackReference
-    @OneToOne
-    @JoinColumn(updatable = false, insertable = false, name = "validationid", referencedColumnName = "validationid")
     private ValidationFormEntity validationFormEntity;
-
-    @Id
-    @Column(name = "validationid", nullable = false)
-    private @NonNull Integer validationID;
-
-    @Column(name = "attributevalue", length = 32, nullable = false)
-    private @NonNull String attributeValue;
-
-    @Column(name = "parameter", length = 32, nullable = false)
-    private @NonNull String parameter;
-
-    @Column(name = "value", length = 32, nullable = false)
-    private @NonNull String value;
-
-    @Column(name = "createdby", length = 16, nullable = false)
-    private @NonNull String createdBy;
-
-    @Column(name = "createddate", length = 7, nullable = false)
-    private @NonNull Timestamp createdDate;
-
-    @Column(name = "lastupdatedby", length = 16)
+    private Integer validationID;
+    private String attributeValue;
+    private String parameter;
+    private String value;
+    private String createdBy;
+    private Timestamp createdDate;
     private String lastUpdatedBy;
-
-    @Column(name = "lastupdateddate", length = 7)
     private Timestamp lastUpdatedDate;
-
 }
