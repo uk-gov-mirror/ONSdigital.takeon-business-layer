ed5648afac23f0bce38be730e68df5cbc1ee126f
mattprice-28 <mattprice_28@hotmail.co.uk>
2019-09-12 13:33:57 +0100

    fixed qlsearch to use qlbuild class

--- src/main/java/uk/gov/ons/collection/controller/ContributorController.java
+++ src/main/java/uk/gov/ons/collection/controller/ContributorController.java
@@ -101,23 +101,9 @@ public class ContributorController {
 
     public String buildQuery(@MatrixVariable Map <String, String> searchParameters){
 
-        String queryPrefix = "{\"query\": \"query contributorSearchBy {" +
-                "allContributors ";
-        String querySuffix = "{" +
-                "nodes {" +
-                "reference, period, survey, formid, status, receiptdate, lockedby, lockeddate}}}\" }";
-
-        StringBuilder builtQuery = new StringBuilder();
-        builtQuery.append(queryPrefix);
-        if (searchParameters != null && !searchParameters.isEmpty()){
-            builtQuery.append("(condition: { ");
-            searchParameters.forEach((key,value) -> builtQuery.append(key + ": \\\"" + value + "\\\" "));
-            builtQuery.append("})");
-        }
-        builtQuery.append(querySuffix);
-        //String testQuery = "{\"query\": \"query contributorSearchBy {allContributors {nodes {reference}}}\"}";
-        System.out.println("Hello " + builtQuery.toString());
-        return qlService.qlSearch(builtQuery.toString());
+        qlQueryBuilder query = new qlQueryBuilder();
+        System.out.println(query.buildQuery(searchParameters));
+        return qlService.qlSearch(query.buildQuery(searchParameters));
     }
 
 }
--- src/main/java/uk/gov/ons/collection/controller/qlQueryBuilder.java
+++ src/main/java/uk/gov/ons/collection/controller/qlQueryBuilder.java
@@ -6,17 +6,17 @@ public class qlQueryBuilder {
 
     public String buildQuery(Map<String, String> searchParameters){
 
-        String queryPrefix = "query contributorSearchBy {" +
+        String queryPrefix = "{\"query\": \"query contributorSearchBy {" +
                 "allContributors ";
         String querySuffix = "{" +
                 "nodes {" +
-                "reference, period, survey, formid, status, receiptdate, lockedby, lockeddate}}}";
+                "reference, period, survey, formid, status, receiptdate, lockedby, lockeddate}}}\" }";
 
         StringBuilder builtQuery = new StringBuilder();
         builtQuery.append(queryPrefix);
         if (searchParameters != null && !searchParameters.isEmpty()){
             builtQuery.append("(condition: { ");
-            searchParameters.forEach((key,value) -> builtQuery.append(key + ": \"" + value + "\" "));
+            searchParameters.forEach((key,value) -> builtQuery.append(key + ": \\\"" + value + "\\\" "));
             builtQuery.append("})");
         }
         builtQuery.append(querySuffix);
