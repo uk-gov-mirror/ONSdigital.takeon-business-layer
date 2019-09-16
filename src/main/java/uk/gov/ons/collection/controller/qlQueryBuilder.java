8ea7f557418cc5a4372d7e02ebecc484ec7fd3f1
David Morgan <github@ghost.me.uk>
2019-09-16 12:57:41 +0100

    Updated QueryBuilder class and tests

--- src/main/java/uk/gov/ons/collection/controller/qlQueryBuilder.java
+++ src/main/java/uk/gov/ons/collection/controller/qlQueryBuilder.java
@@ -4,22 +4,23 @@ import java.util.Map;
 
 public class qlQueryBuilder {
 
-    public String buildContributorSearchQuery(Map<String, String> searchParameters){
-
-        String queryPrefix = "{\"query\": \"query contributorSearchBy {" +
-                "allContributors ";
-        String querySuffix = "{" +
-                "nodes {" +
-                "reference, period, survey, formid, status, receiptdate, lockedby, lockeddate}}}\" }";
-
+    public String buildContributorSearchQuery(Map<String, String> searchParameters) {
         StringBuilder builtQuery = new StringBuilder();
-        builtQuery.append(queryPrefix);
-        if (searchParameters != null && !searchParameters.isEmpty()){
-            builtQuery.append("(condition: { ");
-            searchParameters.forEach((key,value) -> builtQuery.append(key + ": \\\"" + value + "\\\" "));
-            builtQuery.append("})");
-        }
-        builtQuery.append(querySuffix);
+        builtQuery.append("{\"query\": \"query contributorSearchBy { allContributors ");
+        builtQuery.append(buildCondition(searchParameters));
+        builtQuery.append("{ nodes { reference, period, survey, formid, status, receiptdate, lockedby, lockeddate}}}\" }");
         return builtQuery.toString();
+    }
+
+    // Build up a graphQL syntax condition. If invalid (or empty) conditions are provided returns a blank string
+    public String buildCondition(Map<String, String> parameters) {    
+        if (parameters == null || parameters.isEmpty()) {
+            return "";
+        }        
+        StringBuilder conditions = new StringBuilder();
+        conditions.append("(condition: { ");
+        parameters.forEach((key,value) -> conditions.append(key + ": \\\"" + value + "\\\" "));
+        conditions.append("})");
+        return conditions.toString();
     }
 }
--- src/test/java/uk/gov/ons/collection/test/qlQueryBuilderTest.java
+++ src/test/java/uk/gov/ons/collection/test/qlQueryBuilderTest.java
@@ -1,59 +1,74 @@
 package uk.gov.ons.collection.test;
 
-import org.junit.jupiter.api.DisplayName;
-import org.junit.jupiter.api.Test;
 import uk.gov.ons.collection.controller.qlQueryBuilder;
-
+import org.junit.jupiter.api.Test;
 import java.util.HashMap;
 import java.util.Map;
 
 import static org.junit.jupiter.api.Assertions.assertEquals;
 
-@DisplayName("Ql Query Builder tests")
-
 public class qlQueryBuilderTest {
 
-    String baseQuery = "query contributorSearchBy {" +
-            "allContributors {" +
-            "nodes {" +
-            "reference, period, survey, formid, status, receiptdate, lockedby, lockeddate}}}";
-
     @Test
-    void buildQuery_nullParameters_unfilteredQuery(){
-        qlQueryBuilder qlBuild = new qlQueryBuilder();
-        assertEquals(baseQuery,qlBuild.buildContributorSearchQuery(null));
+    void buildQuery_nullParameters_unfilteredQuery() {
+        String expectedQuery = "{\"query\": \"query contributorSearchBy { allContributors { nodes { reference, period, survey, formid, status, receiptdate, lockedby, lockeddate}}}\" }";
+        assertEquals(expectedQuery, new qlQueryBuilder().buildContributorSearchQuery(null));
     }
 
     @Test
     void buildQuery_emptyParameters_unfilteredQuery(){
-        qlQueryBuilder qlBuild = new qlQueryBuilder();
+        String expectedQuery = "{\"query\": \"query contributorSearchBy { allContributors { nodes { reference, period, survey, formid, status, receiptdate, lockedby, lockeddate}}}\" }";
         Map<String,String> emptyParameters = new HashMap<>();
-        assertEquals(baseQuery,qlBuild.buildContributorSearchQuery(emptyParameters));
+        assertEquals(expectedQuery, new qlQueryBuilder().buildContributorSearchQuery(emptyParameters));
     }
 
     @Test
     void buildQuery_singleParameter_filteredQuery(){
-        String builtQuery= "query contributorSearchBy {" +
-                "allContributors (condition: { reference: \"4990012\" }){" +
-                "nodes {" +
-                "reference, period, survey, formid, status, receiptdate, lockedby, lockeddate}}}";
-        qlQueryBuilder qlBuild = new qlQueryBuilder();
+        String expectedQuery= "{\"query\": \"query contributorSearchBy { " +
+                "allContributors (condition: { reference: \\\"4990012\\\" }){ " +
+                "nodes { reference, period, survey, formid, status, receiptdate, lockedby, lockeddate}}}\" }";
         Map<String,String> singleParameter = new HashMap<>();
         singleParameter.put("reference", "4990012");
-        assertEquals(builtQuery,qlBuild.buildContributorSearchQuery(singleParameter));
+        assertEquals(expectedQuery, new qlQueryBuilder().buildContributorSearchQuery(singleParameter));
+    }
+
+    @Test
+    void buildCondition_null_blankString() {
+        assertEquals("", new qlQueryBuilder().buildCondition(null));
+    }
+
+    @Test
+    void buildCondition_emptyParameters_blankString() {
+        Map<String,String> emptyParameter = new HashMap<>();
+        assertEquals("", new qlQueryBuilder().buildCondition(emptyParameter));
+    }
+
+    @Test
+    void buildCondition_singleParameter_singleCondition() {
+        String expectedCondition = "(condition: { reference: \\\"4990012\\\" })";
+        Map<String,String> singleParameter = new HashMap<>();
+        singleParameter.put("reference", "4990012");
+        assertEquals(expectedCondition, new qlQueryBuilder().buildCondition(singleParameter));
+    }
+
+    @Test
+    void buildCondition_twoParameters_twoCondition() {
+        String expectedCondition = "(condition: { reference: \\\"4990012\\\" survey: \\\"011\\\" })";
+        Map<String,String> twoParameters = new HashMap<>();
+        twoParameters.put("reference", "4990012");
+        twoParameters.put("survey", "011");
+        assertEquals(expectedCondition, new qlQueryBuilder().buildCondition(twoParameters));
     }
 
     @Test
     void buildQuery_twoParameters_filteredQuery(){
-        String builtQuery= "query contributorSearchBy {" +
-                "allContributors (condition: { reference: \"4990012\" period: \"201903\" }){" +
-                "nodes {" +
-                "reference, period, survey, formid, status, receiptdate, lockedby, lockeddate}}}";
-        qlQueryBuilder qlBuild = new qlQueryBuilder();
+        String builtQuery= "{\"query\": \"query contributorSearchBy { " +
+                "allContributors (condition: { reference: \\\"4990012\\\" period: \\\"201903\\\" }){ " +
+                "nodes { reference, period, survey, formid, status, receiptdate, lockedby, lockeddate}}}\" }";
         Map<String,String> twoParameters = new HashMap<>();
         twoParameters.put("reference", "4990012");
         twoParameters.put("period", "201903");
-        assertEquals(builtQuery,qlBuild.buildContributorSearchQuery(twoParameters));
+        assertEquals(builtQuery, new qlQueryBuilder().buildContributorSearchQuery(twoParameters));
     }
 
 }
