918e4b0dea1baed31458038336443f3df4d0c96f
David Morgan <github@ghost.me.uk>
2019-09-24 15:21:56 +0100

    Tidied up logging statements

--- src/main/java/uk/gov/ons/collection/controller/ContributorController.java
+++ src/main/java/uk/gov/ons/collection/controller/ContributorController.java
@@ -103,23 +103,19 @@ public class ContributorController {
         log.info("Query sent to service: " + qlQuery);
         try {
             qlQueryResponse response = new qlQueryResponse(qlService.qlSearch(qlQuery));
-	    System.out.println(response.toString());
             responseText = response.parse();
         }
         catch(Exception e){
             responseText = "{\"error\":\"Invalid response from graphQL\"}";
         }
-	System.out.println(responseText.toString());
+        log.info("Query sent to service: " + qlQuery);     
         return responseText;
     }
 
     @GetMapping(value = "/dbExport", produces = MediaType.APPLICATION_JSON_VALUE)
     public String validationDbExport(){
         qlQueryBuilder query = new qlQueryBuilder(null);
-        // log.info("Query sent to GraphQL" );
         String response = qlService.qlSearch(query.buildExportDBQuery());
-        // log.info("Result returned from GraphQL successfully" );
-        // log.info("Response after calling endpoint response { }", response);
         return response;
     }
 }
