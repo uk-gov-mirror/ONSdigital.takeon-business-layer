b23287d8da0ea3b45eb7b36cc735d95e2f918eb3
Siva Gudur <siva@Sivas-MacBook-Pro.local>
2019-11-11 12:36:58 +0000

    Added test for comparison logic

--- src/test/java/uk/gov/ons/collection/test/UpsertResponseTest.java
+++ src/test/java/uk/gov/ons/collection/test/UpsertResponseTest.java
@@ -1,8 +1,14 @@
 package uk.gov.ons.collection.test;
 
+import org.json.JSONArray;
+import org.json.JSONObject;
 import org.junit.jupiter.api.Test;
+import uk.gov.ons.collection.entity.ResponseData;
 import uk.gov.ons.collection.exception.InvalidJsonException;
+import uk.gov.ons.collection.service.CompareUiAndCurrentResponses;
 import uk.gov.ons.collection.utilities.UpsertResponse;
+
+import java.util.List;
 
 import static org.junit.Assert.assertEquals;
 import static org.junit.jupiter.api.Assertions.*;
@@ -102,5 +108,64 @@ class UpsertResponseTest {
                 "\"survey\": \"999A\",\"questioncode\": \"1000\",\"instance\": 0}]}";
         assertThrows(InvalidJsonException.class, () -> new UpsertResponse(inputJson).buildUpsertByArrayQuery());
     }
+
+    @Test
+    void testCompareUiAndCurrentResponses() {
+
+        //UI Responses
+        String json1 = "{\"responses\": [{\"question\": \"1000\", \"response\": \"1\", \"instance\": 0}, " +
+                "{\"question\": \"1001\", \"response\": \"2\", \"instance\": 0}, " +
+                "{\"question\": \"2000\", \"response\": \"3\", \"instance\": 0}, " +
+                "{\"question\": \"3000\", \"response\": \"\", \"instance\": 0}, " +
+                "{\"question\": \"4000\", \"response\": \"\", \"instance\": 0}, " +
+                "{\"question\": \"4001\", \"response\": \"\", \"instance\": 0}]," +
+                " \"user\": \"fisdba\", \"reference\": \"12345678000\", \"period\": \"201801\", \"survey\": \"999A\"}";
+        //Old Responses
+        String json2 = "[{\"reference\":\"12345678000\",\"period\":\"201801\",\"instance\":0,\"createdby\":\"fisdba\"," +
+                "\"createddate\":\"2019-11-08T16:11:16.009+00:00\",\"lastupdateddate\":\"2019-11-08T16:11:16.001+00:00\"," +
+                "\"response\":\"1\",\"questioncode\":\"1000\",\"lastupdatedby\":\"fisdba\",\"survey\":\"999A\"}," +
+                "{\"reference\":\"12345678000\",\"period\":\"201801\",\"instance\":0,\"createdby\":\"fisdba\"," +
+                "\"createddate\":\"2019-11-08T16:11:16.009+00:00\",\"lastupdateddate\":\"2019-11-08T16:11:16.001+00:00\"," +
+                "\"response\":\"1\",\"questioncode\":\"1001\",\"lastupdatedby\":\"fisdba\",\"survey\":\"999A\"}" +
+                "]";
+
+        String expectedCondition = "{reference: \\\"12345678000\\\",period: \\\"201801\\\",survey: \\\"999A\\\",questioncode: \\\"1001\\\"," +
+                "instance: 0,response: \\\"2\\\"";
+        String expectedCondition1 = "{reference: \\\"12345678000\\\",period: \\\"201801\\\",survey: \\\"999A\\\",questioncode: \\\"1000\\\"," +
+                "instance: 0,response: \\\"1\\\"";
+
+        int responseChangeCount = 2;
+
+        List<ResponseData> currentResponseEntities;
+        JSONObject updatedResponsesJson = new JSONObject(json1);
+        CompareUiAndCurrentResponses responseComparison;
+
+        try {
+            var upsertResponse = new UpsertResponse(json1);
+            var outputArray = new JSONArray(json2);
+            currentResponseEntities = upsertResponse.buildCurrentResponseEntities(outputArray);
+            responseComparison = new CompareUiAndCurrentResponses(currentResponseEntities, updatedResponsesJson);
+            List<ResponseData> responsesToPassToDatabase = responseComparison.getFinalConsolidatedResponses();
+            //Veryfying how many number of elements changed.
+            assertEquals(responseChangeCount, responsesToPassToDatabase.size());
+
+            JSONArray jsonArray = new JSONArray(responsesToPassToDatabase);
+            var upsertSaveResponse = new UpsertResponse(jsonArray);
+            var saveQuery = upsertSaveResponse.buildConsolidateUpsertByArrayQuery();
+            //Save Query contains current updated date. So I cannot compare the actual query with expected query.
+            // Verifying whether the query which was built has database function or not
+            assertTrue(saveQuery.contains("mutation saveResponse {saveresponsearray(input: {arg0:"));
+            //The element Question Code 1001 changed so it should be in Save Query
+            assertTrue(saveQuery.contains(expectedCondition));
+            //The element Question Code 1000 has not changed and it should not be in Save Query
+            assertFalse(saveQuery.contains(expectedCondition1));
+
+        } catch (Exception e) {
+            assertTrue(false);
+        }
+
+    }
+
+
 
 }
