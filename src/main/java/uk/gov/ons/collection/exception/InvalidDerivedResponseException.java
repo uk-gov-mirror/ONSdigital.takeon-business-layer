0fbefc3d3871e0cda489bfd75d8f2dbadc522d06
RyanAldy <ryanalderson91@gmail.com>
2019-11-19 08:45:04 +0000

    Added generated serial version ID

--- src/main/java/uk/gov/ons/collection/exception/InvalidDerivedResponseException.java
+++ src/main/java/uk/gov/ons/collection/exception/InvalidDerivedResponseException.java
@@ -2,7 +2,7 @@ package uk.gov.ons.collection.exception;
 
 public class InvalidDerivedResponseException extends Exception { 
     
-    private static final long serialVersionUID = 1L;
+    private static final long serialVersionUID = -3474879265386733983L;
 
     public InvalidDerivedResponseException(String errorMessage, Throwable err) {
         super(errorMessage, err);
