1ed75f397dc903b44d5771c664d42110e76746a4
RyanAldy <ryanalderson91@gmail.com>
2020-08-12 10:32:58 +0100

    Added in selection file columns

--- src/main/java/uk/gov/ons/collection/utilities/SelectionFileQuery.java
+++ src/main/java/uk/gov/ons/collection/utilities/SelectionFileQuery.java
@@ -1,7 +1,9 @@
 package uk.gov.ons.collection.utilities;
 
+import java.sql.Timestamp;
 import java.util.ArrayList;
 import java.util.Arrays;
+import java.util.Date;
 import java.util.HashMap;
 import java.util.List;
 import java.util.Map;
@@ -16,8 +18,9 @@ import uk.gov.ons.collection.exception.InvalidJsonException;
 public class SelectionFileQuery {
     private HashMap<String, String> variables;
     private List<String> intVariables = new ArrayList<>(Arrays.asList("first", "last", "formid"));
-    private JSONArray responseArray;
-    private JSONObject responseObject;
+    private JSONArray contributorValuesArray;
+    private JSONObject contributorObject;
+    private final Timestamp time = new Timestamp(new Date().getTime());
 
     public SelectionFileQuery(Map<String, String> variables){
         this.variables = (variables == null) ? new HashMap<>() : new HashMap<>(variables);
@@ -25,8 +28,8 @@ public class SelectionFileQuery {
 
     public SelectionFileQuery(String jsonString) throws InvalidJsonException {
         try {
-            responseObject = new JSONObject(jsonString);
-            responseArray = responseObject.getJSONArray("responses");
+            contributorObject = new JSONObject(jsonString);
+            contributorValuesArray = contributorObject.getJSONArray("Values");
 
         } catch (JSONException err) {
             throw new InvalidJsonException("Given string could not be converted/processed: " + jsonString, err);
@@ -67,31 +70,76 @@ public class SelectionFileQuery {
     // Loop through the given validation output array json and convert it into a graphQL compatable format
     private String getSelectionLoadData() throws InvalidJsonException {
         StringJoiner joiner = new StringJoiner(",");
-        for (int i = 0; i < responseArray.length(); i++) {
+        for (int i = 0; i < contributorValuesArray.length(); i++) {
             joiner.add("{" + extractValidationOutputRow(i) + "}");
         }
         return joiner.toString();
+    }
+
+    public String getTime() {
+        return time.toString();
     }
 
     // Convert a row for the given index and provide it in graphQL desired format
     private String extractValidationOutputRow(int index) throws InvalidJsonException {
         StringJoiner joiner = new StringJoiner(",");
         try {
-            var outputRow = responseArray.getJSONObject(index);
-            var refPerSur = responseObject;
-            joiner.add("reference: \\\"" + refPerSur.getString("reference") + "\\\"");
-            joiner.add("period: \\\"" + refPerSur.getString("period") + "\\\"");
-            joiner.add("survey: \\\"" + refPerSur.getString("survey") + "\\\"");
-            joiner.add("questioncode: \\\"" + outputRow.getString("questioncode") + "\\\"");
-            joiner.add("instance: " + outputRow.getInt("instance"));
-            joiner.add("response: \\\"" + outputRow.getString("response") + "\\\"");
-            joiner.add("createdby: \\\"fisdba\\\"");
-            joiner.add("createddate: \\\"" + time.toString() + "\\\"");
-            joiner.add("lastupdatedby: \\\"fisdba\\\"");
-            joiner.add("lastupdateddate: \\\"" + time.toString() + "\\\"");
+            var outputRow = contributorValuesArray.getJSONObject(index);
+            var periodSurvey = contributorObject;
+            joiner.add("Period: \\\"" + periodSurvey.getString("Period") + "\\\"");
+            joiner.add("Survey: \\\"" + periodSurvey.getString("Survey_Code") + "\\\"");
+            joiner.add("reference: \\\"" + outputRow.getString("ruref") + "\\\"");
+            joiner.add("FormID: " + outputRow.getInt("formtype")); // Call a method here to get the form id or pass it through into API for this call?
+            joiner.add("Status: \\\" \\\"");
+            joiner.add("ReceiptDate: \\\" \\\"");
+            joiner.add("LockedBy: \\\" \\\"");
+            joiner.add("LockedDate: \\\" \\\"");
+            joiner.add("FormType: \\\"" + outputRow.getString("formtype"));
+            joiner.add("Checkletter: \\\"" + outputRow.getString("checkletter"));
+            joiner.add("FrozenSicOutdated: \\\"" + outputRow.getString("frosic92"));
+            joiner.add("RuSicOutdated: \\\"" + outputRow.getString("rusic92"));
+            joiner.add("FrozenSic: \\\"" + outputRow.getString("frosic2007"));
+            joiner.add("RuSic: \\\"" + outputRow.getString("rusic2007"));
+            joiner.add("FrozenEmployees: \\\"" + outputRow.getString("froempees"));
+            joiner.add("Employees: \\\"" + outputRow.getString("employees"));
+            joiner.add("FrozenEmployment: \\\"" + outputRow.getString("froempment"));
+            joiner.add("Employment: \\\"" + outputRow.getString("employment"));
+            joiner.add("FrozenFteEmployment: \\\"" + outputRow.getString("froftempt"));
+            joiner.add("FteEmployment: \\\"" + outputRow.getString("ftempment"));
+            joiner.add("FrozenTurnover: \\\"" + outputRow.getString("frotover"));
+            joiner.add("Turnover: \\\"" + outputRow.getString("turnover"));
+            joiner.add("EnterpriseReference: \\\"" + outputRow.getString("entref"));
+            joiner.add("WowEnterpriseReference: \\\"" + outputRow.getString("wowentref"));
+            joiner.add("CellNumber: \\\"" + outputRow.getString("cell_no"));
+            joiner.add("Currency: \\\"" + outputRow.getString("currency"));
+            joiner.add("VatReference: \\\"" + outputRow.getString("vatref"));
+            joiner.add("PayeReference: \\\"" + outputRow.getString("payeref"));
+            joiner.add("CompanyRegistrationNumber: \\\"" + outputRow.getString("crn"));
+            joiner.add("NumberLiveLocalUnits: \\\"" + outputRow.getString("live_lu"));
+            joiner.add("NumberLiveVat: \\\"" + outputRow.getString("live_vat"));
+            joiner.add("NumberLivePaye: \\\"" + outputRow.getString("live_paye"));
+            joiner.add("LegalStatus: \\\"" + outputRow.getString("legalstatus"));
+            joiner.add("ReportingUnitMarker: \\\"" + outputRow.getString("entrepmkr"));
+            joiner.add("Region: \\\"" + outputRow.getString("region"));
+            joiner.add("BirthDate: \\\"" + outputRow.getString("birthdate"));
+            joiner.add("EnterpriseName: \\\"" + outputRow.getString("entname1") + outputRow.getString("entname2") + outputRow.getString("entname3"));
+            joiner.add("ReferenceName: \\\"" + outputRow.getString("runame1") + outputRow.getString("runame2") + outputRow.getString("runame3"));
+            joiner.add("ReferenceAddress: \\\"" + outputRow.getString("ruaddr1") + outputRow.getString("ruaddr2") + outputRow.getString("ruaddr3") + 
+                                                outputRow.getString("ruaddr4") + outputRow.getString("ruaddr5"));
+            joiner.add("ReferencePostcode: \\\"" + outputRow.getString("rupostcode"));
+            joiner.add("TradingStyle: \\\"" + outputRow.getString("tradstyle1") + outputRow.getString("tradstyle2") + outputRow.getString("tradstyle3"));
+            joiner.add("Contact: \\\"" + outputRow.getString("contact"));
+            joiner.add("Telephone: \\\"" + outputRow.getString("telephone"));
+            joiner.add("Fax: \\\"" + outputRow.getString("fax"));
+            joiner.add("SelectionType: \\\"" + outputRow.getString("seltype"));
+            joiner.add("InclusionExclusion: \\\"" + outputRow.getString("inclexcl"));
+            joiner.add("CreatedBy: \\\"fisdba\\\"");
+            joiner.add("CreatedDate: \\\"" + time.toString() + "\\\"");
+            joiner.add("LastUpdatedBy: \\\" \\\"");
+            joiner.add("LastUpdatedDate: \\\" \\\"");
             return joiner.toString();
         } catch (Exception err) {
-            throw new InvalidJsonException("Error processing response json structure: " + responseArray, err);
+            throw new InvalidJsonException("Error processing response json structure: " + contributorValuesArray, err);
         }
     }
 
