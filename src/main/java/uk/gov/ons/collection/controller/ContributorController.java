69692796d738b29cda2c49f30af16db94e9cb52e
RyanAldy <ryanalderson91@gmail.com>
2020-08-13 14:46:01 +0100

    Added ResponseBody annotation

--- src/main/java/uk/gov/ons/collection/controller/ContributorController.java
+++ src/main/java/uk/gov/ons/collection/controller/ContributorController.java
@@ -6,25 +6,22 @@ import io.swagger.annotations.ApiResponse;
 import io.swagger.annotations.ApiResponses;
 import lombok.extern.log4j.Log4j2;
 
-import org.apache.commons.configuration.interpol.ExprLookup.Variables;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.MediaType;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.MatrixVariable;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.ResponseBody;
 import org.springframework.web.bind.annotation.RestController;
 import uk.gov.ons.collection.entity.FullDataExport;
 import uk.gov.ons.collection.service.GraphQlService;
 import uk.gov.ons.collection.utilities.QlQueryBuilder;
 import uk.gov.ons.collection.utilities.QlQueryResponse;
 import uk.gov.ons.collection.utilities.SelectionFileQuery;
-import uk.gov.ons.collection.utilities.SelectionFileResponse;
 
 import java.util.Map;
-import java.util.StringJoiner;
 
-import com.jayway.jsonpath.InvalidJsonException;
 
 @Log4j2
 @Api(value = "Contributor Controller", description = "End point for the repository data related to contributors")
@@ -38,17 +35,17 @@ public class ContributorController {
     @GetMapping(value = "/qlSearch/{vars}", produces = MediaType.APPLICATION_JSON_VALUE)
     @ApiResponses(value = {
             @ApiResponse(code = 200, message = "Successful retrieval of Contributor details")})
-    public String searchContributor(@MatrixVariable Map<String, String> searchParameters) {
-        String qlQuery = new QlQueryBuilder(searchParameters).buildContributorSearchQuery();
+    public String searchContributor(@MatrixVariable final Map<String, String> searchParameters) {
+        final String qlQuery = new QlQueryBuilder(searchParameters).buildContributorSearchQuery();
         String responseText;
         log.info("Query sent to service: " + qlQuery);
         try {
-            QlQueryResponse response = new QlQueryResponse(qlService.qlSearch(qlQuery));
+            final QlQueryResponse response = new QlQueryResponse(qlService.qlSearch(qlQuery));
             responseText = response.parse();
-        } catch (Exception e) {
+        } catch (final Exception e) {
             responseText = "{\"error\":\"Invalid response from graphQL\"}";
         }
-        log.info("Query sent to service: " + qlQuery);     
+        log.info("Query sent to service: " + qlQuery);
         return responseText;
     }
 
@@ -58,47 +55,53 @@ public class ContributorController {
         var response = "";
         try {
             response = qlService.qlSearch(new FullDataExport().buildQuery());
-        } catch (Exception e) {
-            log.info("Exception: " + e);     
+        } catch (final Exception e) {
+            log.info("Exception: " + e);
             log.info("QL Response: " + response);
             return "{\"error\":\"Error loading data for db Export\"}";
         }
         return response;
     }
-    
 
     // @Autowired
-    // @ApiOperation(value = "Check IDBR table and get existing FormID from IDBR form Type", response = String.class)
-    // @GetMapping(value = "/getFormid/{vars}", produces = MediaType.APPLICATION_JSON_VALUE)
-    // @ApiResponses(value = {@ApiResponse(code = 200, message = "Successful retrieval of form id from IDBR form mapping table", response = String.class)})
+    // @ApiOperation(value = "Check IDBR table and get existing FormID from IDBR
+    // form Type", response = String.class)
+    // @GetMapping(value = "/getFormid/{vars}", produces =
+    // MediaType.APPLICATION_JSON_VALUE)
+    // @ApiResponses(value = {@ApiResponse(code = 200, message = "Successful
+    // retrieval of form id from IDBR form mapping table", response =
+    // String.class)})
     // public String getFormid(@MatrixVariable Map<String, String> params) {
 
-    //     log.info("API CALL!! --> /contributor/getFormid/{vars} :: " + params);
-    //     String formIdQuery = "";
-    //     Integer formId;
-    //     try {
-    //         formIdQuery = new SelectionFileQuery(params).buildCheckIDBRFormidQuery();
-    //         SelectionFileResponse response = new SelectionFileResponse(qlService.qlSearch(formIdQuery));
-    //         formId = response.parseFormidResponse();
-    //     } catch (InvalidJsonException err) {
-    //         log.info("Exception found: " + err);
-    //         return "{\"error\":\"Unable to determine selection data\"}";
-    //     }
-    //     log.info("API Complete --> /contributor/getExistingFormid/{vars}");
-    //     return formId.toString();
+    // log.info("API CALL!! --> /contributor/getFormid/{vars} :: " + params);
+    // String formIdQuery = "";
+    // Integer formId;
+    // try {
+    // formIdQuery = new SelectionFileQuery(params).buildCheckIDBRFormidQuery();
+    // SelectionFileResponse response = new
+    // SelectionFileResponse(qlService.qlSearch(formIdQuery));
+    // formId = response.parseFormidResponse();
+    // } catch (InvalidJsonException err) {
+    // log.info("Exception found: " + err);
+    // return "{\"error\":\"Unable to determine selection data\"}";
+    // }
+    // log.info("API Complete --> /contributor/getExistingFormid/{vars}");
+    // return formId.toString();
     // }
 
     @Autowired
     @ApiOperation(value = "Load Selection File for a Survey/Period", response = String.class)
     @GetMapping(value = "/loadSelectionFile", produces = MediaType.APPLICATION_JSON_VALUE)
-    @ApiResponses(value = {@ApiResponse(code = 200, message = "Successful load of Selection file", response = String.class)})
+    @ApiResponses(value = {
+            @ApiResponse(code = 200, message = "Successful load of Selection file", response = String.class) })
+    @ResponseBody
     public String loadSelectionFile(@RequestBody String selectionData) {
         log.info("API CALL!! --> /contributor/loadSelectionFile:: ");
         try {
             var loadQuery = new SelectionFileQuery(selectionData).buildSaveSelectionFileQuery();
             qlService.qlSearch(loadQuery);
 
-        } catch (Exception e) {
+        } catch (final Exception e) {
             log.info("Can't build Batch Selection Load Query / Invalid Response from GraphQL: " + e);
             return "{\"error\":\"Failed to load Selection File\"}";
         }
